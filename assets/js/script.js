b61e4ee325819d6477e7c1fd6defdfb420ee4be9
lpewton <liampewton99@gmail.com>
2022-11-29 12:09:38 +0000

    Add code to generate random card for player

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+document.getElementById("take-one").addEventListener ("click" , takeOne);
+
+/**
+ *  Gives player 1 card and Computer another
+ */
+function takeOne() {
+    let card = Math.floor(Math.random()*6);
+    console.log(card);
+
+    let firstCard = document.getElementById("your-hand");
+    firstCard.setAttribute('src', `/assets/images/${card}.png`);
+};
