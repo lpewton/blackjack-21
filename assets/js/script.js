d625400793d1658bc8de4c17d326b7ae864584a7
lpewton <liampewton99@gmail.com>
2022-11-29 23:29:23 +0000

    Add computerTakesOne function to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,36 @@ document.getElementById("take-one").addEventListener ("click" , takeOne);
 /**
  *  Gives player 1 card and Computer another
  */
+
+let playerCards = [];
+let computerCards = [];
+
+
 function takeOne() {
-    let card = Math.floor(Math.random()*6);
-    console.log(card);
-    let firstCard = document.getElementById("your-hand");
-    firstCard.setAttribute('src', `/assets/images/${card}.png`);
-};
+
+    let card = Math.floor(Math.random()*12);
+
+    if (playerCards.includes(card) || computerCards.includes(card)) {
+        takeOne();
+    } else {
+        let firstCard = document.getElementById("player-hand");
+        firstCard.setAttribute('src', `/assets/images/${card}.png`);
+        playerCards.push(card);
+        computerTakesOne();
+        console.log(playerCards);
+    }
+}
+
+/**
+ * Give a card to computer and store its value to an array
+ */
+function computerTakesOne() {
+    let card = Math.floor(Math.random()*12);
+
+    if (playerCards.includes(card) || computerCards.includes(card)) {
+        computerTakesOne();
+    } else {
+        computerCards.push(card);
+        console.log(computerCards);
+    }
+}
