4e2c4125449bbb1f015afe1aa32fc4e71b923b09
lpewton <liampewton99@gmail.com>
2022-11-30 01:03:43 +0000

    Add ca

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,34 @@
 document.getElementById("take-one").addEventListener ("click" , takeOne);
 
-/**
- *  Gives player 1 card and Computer another
- */
-
 let playerCards = [];
 let computerCards = [];
 
-
+/**
+ *  Gives player 1 card and stores it into an array
+ */
 function takeOne() {
 
-    let card = Math.floor(Math.random()*12);
+    let card = Math.floor(Math.random()*51);
 
     if (playerCards.includes(card) || computerCards.includes(card)) {
         takeOne();
     } else {
         let firstCard = document.getElementById("player-hand");
-        firstCard.setAttribute('src', `/assets/images/${card}.png`);
+        firstCard.setAttribute('src', `assets/images/${card}.png`);
         playerCards.push(card);
         computerTakesOne();
-        console.log(playerCards);
     }
 }
 
 /**
- * Give a card to computer and store its value to an array
+ * Give a card to computer and store its value into an array
  */
 function computerTakesOne() {
-    let card = Math.floor(Math.random()*12);
+    let card = Math.floor(Math.random()*51);
 
     if (playerCards.includes(card) || computerCards.includes(card)) {
         computerTakesOne();
     } else {
         computerCards.push(card);
-        console.log(computerCards);
     }
 }
