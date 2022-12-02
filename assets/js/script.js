a1d112707b5e8d799b69ff58fbe1bdb03bfed7fa
lpewton <liampewton99@gmail.com>
2022-12-02 00:14:45 +0000

    Add countCards function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,66 @@
 document.getElementById("take-one").addEventListener ("click" , takeOne);
 
-let playerCards = [];
-let computerCards = [];
+let playerCards = 0;
+let computerCards = 0;
+let usedCards = [];
 
 /**
  *  Gives player 1 card and stores it into an array
  */
 function takeOne() {
 
-    let card = Math.floor(Math.random()*51);
+    let card = Math.floor(Math.random()*52);
+    let playerHandDiv = document.getElementById("player-hand-div");
 
-    if (playerCards.includes(card) || computerCards.includes(card)) {
+    if (usedCards.includes(card)) {
         takeOne();
     } else {
-        let firstCard = document.getElementById("player-hand");
+        let firstCard = document.createElement("img");
+        playerHandDiv.appendChild(firstCard);
+        firstCard.setAttribute("class" , "player-hand");
         firstCard.setAttribute('src', `assets/images/${card}.png`);
-        playerCards.push(card);
+
+        playerCards = playerCards + Math.ceil(card / 4 + 0.1);
+        usedCards.push(card);
+
         computerTakesOne();
+        countCards();
+
+        console.log("player: " + playerCards);
     }
 }
 
 /**
- * Give a card to computer and store its value into an array
+ * Gives a card to computer and stores its value into an array
  */
 function computerTakesOne() {
-    let card = Math.floor(Math.random()*51);
+    let card = Math.floor(Math.random()*52);
+    let computerHandDiv = document.getElementById("computer-hand-div");
 
-    if (playerCards.includes(card) || computerCards.includes(card)) {
+    if (usedCards.includes(card)) {
         computerTakesOne();
     } else {
-        computerCards.push(card);
+        let firstCard = document.createElement("img");
+        computerHandDiv.appendChild(firstCard);
+        firstCard.setAttribute("id" , "computer-hand")
+        firstCard.setAttribute('src', `assets/images/reverse.png`);
+        
+        computerCards = computerCards + Math.ceil(card / 4 + 0.1);
+        usedCards.push(card);
+
+        countCards();
+
+        console.log("computer:" + computerCards);
+        console.log("used:" + usedCards);
     }
 }
+
+/**
+ * Counts the cards in each hand
+ */
+function countCards(sum1, sum2) {
+    if (playerCards > 21) {
+        alert("Oh no! You lost!")
+    }
+}
+
