6d44c55fa1b0ee7b136d4f4782bde5da4132bc89
lpewton <liampewton99@gmail.com>
2022-12-03 23:41:06 +0000

    Add code to stop computerTakesOne function and remove buttons when each player reaches 21

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ document.getElementById("take-one").addEventListener ("click" , takeOne);
 let playerCards = [];
 let computerCards = [];
 let usedCards = [];
+let computerValue = 0;
+let playerValue = 0;
 
 /**
  *  Gives player 1 card and stores it into an array
@@ -35,46 +37,53 @@ function takeOne() {
     let card = Math.floor(Math.random()*52);
     let computerHandDiv = document.getElementById("computer-hand-div");
 
-    if (usedCards.includes(card)) {
-        computerTakesOne();
-    } else {
-        let secondCard = document.createElement("img");
-        computerHandDiv.appendChild(secondCard);
-        secondCard.setAttribute("class" , "computer-hand")
-        secondCard.setAttribute('src', `assets/images/reverse.png`);
-        
-        usedCards.push(card);
-        computerCards.push(card);
-
-        if (playerCards < 21) {
-            countComputerCards();
+    if (computerValue < 17) {
+        if (usedCards.includes(card)) {
+         computerTakesOne();
         } else {
-            endGame();
+            let secondCard = document.createElement("img");
+            computerHandDiv.appendChild(secondCard);
+            secondCard.setAttribute("class" , "computer-hand")
+            secondCard.setAttribute('src', `assets/images/reverse.png`);
+
+            usedCards.push(card);
+            computerCards.push(card);
         }
+    } else if (computerValue > 17) {
+        window.computerTakesOne = null;
     }
+
+    countComputerCards();
 }
+
 
 /**
  * Counts the cards in the player's hand
  */
  function countPlayerCards() {
 
+    playerValue = 0;
     playerCards.sort();
-
-    let value = 0;
 
     for (let i = 0; i < playerCards.length; i++) {
         if (playerCards[i] <= 31) {
-            value = value + Math.ceil(playerCards[i] / 4 + 1.1);
+            playerValue = playerValue + Math.ceil(playerCards[i] / 4 + 1.1);
         } else if (playerCards[i] >= 48) {
-            if ((value + 11) < 30) {
-                value = value + 11;
-            } else if ((value + 11) > 30){
-                value = value + 1;
+            if ((playerValue + 11) < 30) {
+                playerValue = playerValue + 11;
+            } else if ((playerValue + 11) > 30){
+                playerValue = playerValue + 1;
             }
         } else if (32 <= playerCards[i] <= 47) {
-            value = value + 10;
+            playerValue = playerValue + 10;
         }
+    }
+
+    if (playerValue > 21) {
+      let takeOne = document.getElementsByTagName('button')[0];
+      let fold = document.getElementsByTagName('button')[1];
+      takeOne.remove();
+      fold.remove();
     }
  }
 
@@ -83,21 +92,20 @@ function takeOne() {
  */
   function countComputerCards() {
 
+    computerValue = 0;
     computerCards.sort();
-
-    let value = 0;
 
     for (let i = 0; i < computerCards.length; i++) {
         if (computerCards[i] <= 31) {
-            value = value + Math.ceil(computerCards[i] / 4 + 1.1);
+            computerValue = computerValue + Math.ceil(computerCards[i] / 4 + 1.1);
         } else if (computerCards[i] >= 48) {
-            if ((value + 11) < 30) {
-                value = value + 11;
-            } else if ((value + 11) > 30){
-                value = value + 1;
+            if ((computerValue + 11) < 21) {
+                computerValue = computerValue + 11;
+            } else if ((computerValue + 11) > 21){
+                computerValue = computerValue + 1;
             }
         } else if (32 <= computerCards[i] <= 47) {
-            value = value + 10;
+            computerValue = computerValue + 10;
         }
     }
- }
+}
