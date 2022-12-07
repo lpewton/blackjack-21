f3d96bf19024d0a94ef30165faa263046e1a6b08
lpewton <liampewton99@gmail.com>
2022-12-07 02:12:57 +0000

    Create fold function and code to display score at the end of the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 document.getElementById("take-one").addEventListener ("click" , takeOne);
+document.getElementById("fold").addEventListener ("click" , fold);
 
 let playerCards = [];
 let computerCards = [];
@@ -11,25 +12,30 @@ let playerValue = 0;
  */
 function takeOne() {
 
-    let card = Math.floor(Math.random() * 52);
+    let cardNum = Math.floor(Math.random() * 52);
     let playerHandDiv = document.getElementById("player-hand-div");
 
     playerHandDiv.style.backgroundColor = "#116D31";
 
-    if (usedCards.includes(card)) {
+    if (usedCards.includes(cardNum)) {
         takeOne();
     } else {
-        let firstCard = document.createElement("img");
-        playerHandDiv.appendChild(firstCard);
-        firstCard.setAttribute("class" , "player-hand");
-        firstCard.setAttribute('src', `assets/images/${card}.png`);
-    }
-    
-    usedCards.push(card);
-    playerCards.push(card);
+        let card = document.createElement("img");
+        playerHandDiv.appendChild(card);
+        card.setAttribute("class" , "player-hand");
+        card.setAttribute('src', `assets/images/${cardNum}.png`);
 
-    computerTakesOne();
-    countPlayerCards();
+        usedCards.push(cardNum);
+        playerCards.push(cardNum);
+
+        playerCards.sort();
+
+        if (computerValue < 17) {
+        computerTakesOne();
+        }
+
+        countPlayerCards();
+    }
 }
 
 /**
@@ -43,7 +49,7 @@ function takeOne() {
 
     if (computerValue < 17) {
         if (usedCards.includes(card)) {
-         computerTakesOne();
+        computerTakesOne();
         } else {
             let secondCard = document.createElement("img");
             computerHandDiv.appendChild(secondCard);
@@ -52,14 +58,11 @@ function takeOne() {
 
             usedCards.push(card);
             computerCards.push(card);
+
+            countComputerCards();
         }
-    } else if (computerValue > 17) {
-        window.computerTakesOne = null;
     }
-
-    countComputerCards();
 }
-
 
 /**
  * Counts the cards in the player's hand
@@ -67,13 +70,12 @@ function takeOne() {
  function countPlayerCards() {
 
     playerValue = 0;
-    playerCards.sort();
 
     for (let i = 0; i < playerCards.length; i++) {
         if (playerCards[i] <= 31) {
             playerValue = playerValue + Math.ceil(playerCards[i] / 4 + 1.1);
         } else if (playerCards[i] >= 48) {
-            if ((playerValue + 11) < 21) {
+            if ((playerValue + 11) <= 21) {
                 playerValue = playerValue + 11;
             } else if ((playerValue + 11) > 21){
                 playerValue = playerValue + 1;
@@ -90,12 +92,12 @@ function takeOne() {
       takeOne.remove();
       fold.remove();
 
-      let losingDiv = document.getElementById("losing-div");
+      let losingDiv = document.getElementById("announcements-div");
       let losingHeading = document.createElement("h1");
       losingHeading.innerHTML = `Oh no! Your score is ${playerValue}... You lost!`;
       losingDiv.appendChild(losingHeading);
         }
- }
+    }
 
  /**
  * Counts the cards in the computer's hand
@@ -108,8 +110,8 @@ function takeOne() {
     for (let i = 0; i < computerCards.length; i++) {
         if (computerCards[i] <= 31) {
             computerValue = computerValue + Math.ceil(computerCards[i] / 4 + 1.1);
-        } else if (computerCards[i] >= 48) {
-            if ((computerValue + 11) < 21) {
+        } else if (computerValue[i] >= 48) {
+            if ((computerValue + 11) <= 21) {
                 computerValue = computerValue + 11;
             } else if ((computerValue + 11) > 21){
                 computerValue = computerValue + 1;
@@ -118,4 +120,67 @@ function takeOne() {
             computerValue = computerValue + 10;
         }
     }
+  }
+
+  /**
+   * Gives out each player's score and determines the winner
+   */
+function fold() {
+    let takeOneButton = document.getElementsByTagName('button')[0];
+    let foldButton = document.getElementsByTagName('button')[1];
+
+    takeOneButton.remove();
+    foldButton.remove();
+
+    for (let i = 0; i < computerCards.length; i++) {
+    let card = document.getElementsByClassName("computer-hand")[i];
+
+    card.setAttribute('src', `assets/images/${computerCards[i]}.png`);
+    }
+
+    let computerResult = document.getElementById("computer-value");
+    computerResult.innerHTML = `${computerValue}`;
+
+    let playerResult = document.getElementById("player-value");
+    playerResult.innerHTML = `${playerValue}`;
+
+    if (computerValue > 21) {
+        computerResult.style.color = "red";
+        playerResult.style.color = "green";
+    } else if (playerValue > computerValue){
+        computerResult.style.color = "red";
+        playerResult.style.color = "green";
+    } else if (playerValue < computerValue) {
+        computerResult.style.color = "green";
+        playerResult.style.color = "red";
+    } else if (playerResult === computerResult) {
+        computerResult.style.color = "gray";
+        playerResult.style.color = "gray";
+    }
+
+    if (computerResult.style.color === "red") {
+        let winDiv = document.getElementById("announcements-div");
+        let winHeading = document.createElement("h1");
+        winHeading.style.color = "black";
+        winHeading.innerHTML = `You won!`;
+        winDiv.appendChild(winHeading);
+    }
+
+    if (computerResult.style.color === "green") {
+        let loseDiv = document.getElementById("announcements-div");
+        let loseHeading = document.createElement("h1");
+        loseHeading.style.color = "red";
+        loseHeading.innerHTML = `You lost...`;
+        loseDiv.appendChild(loseHeading);
+    }
+
+    if (computerResult.style.color === "gray") {
+        let drawDiv = document.getElementById("announcements-div");
+        let drawHeading = document.createElement("h1");
+        drawHeading.style.color = "gray";
+        drawHeading.innerHTML = "Draw!"
+        drawDiv.appendChild(drawHeading);
+    }
+
 }
+
